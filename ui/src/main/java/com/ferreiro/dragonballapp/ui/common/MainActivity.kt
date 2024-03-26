b5ea1e97feac0393f45aa05e5ea4f2ff42f31b01
Carlos Ferreiro <cferreirobarros@gmail.com>
2024-03-26 01:49:04 +0100

    Bloqueado modo oscuro

--- ui/src/main/java/com/ferreiro/dragonballapp/ui/common/MainActivity.kt
+++ ui/src/main/java/com/ferreiro/dragonballapp/ui/common/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.ImageButton
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.lifecycleScope
 import com.example.ui.R
 import com.example.ui.databinding.ActivityMainBinding
@@ -19,6 +20,7 @@ class MainActivity: AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
