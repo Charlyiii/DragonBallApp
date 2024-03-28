7306dbce2106ce987aa30e141664f1a991ef8fe7
Carlos Ferreiro <cferreirobarros@gmail.com>
2024-03-28 23:55:48 +0100

    Added font and apply to toolbar

---
+++ ui/src/main/java/com/ferreiro/dragonballapp/ui/theme/Type.kt
@@ -0,0 +1,56 @@
+package com.ferreiro.dragonballapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.ui.R
+
+val dbz_font = FontFamily(
+    Font(R.font.saiyan_sans, FontWeight.Normal),
+    Font(R.font.saiyan_sans_left_oblique, FontWeight.Normal),
+    Font(R.font.saiyan_sans_right_oblique, FontWeight.Normal)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    titleMedium = TextStyle(
+        fontFamily = dbz_font,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = dbz_font,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = dbz_font,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
